2cdfd300767a6a2dc611c0c5e851ed3bd935f7c9
DerTeufel <dominik-kassel@gmx.de>
2013-02-18 19:33:32 +0100

    option to don't resore settings at boot
    Change-Id: I2a4b2433c3c547d926ea70882a5438d0071672b1
    
    Conflicts:
    
    	AriesParts/res/xml/main.xml
    	AriesParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
    	AriesParts/src/com/cyanogenmod/settings/device/Startup.java

--- AriesParts/src/com/cyanogenmod/settings/device/Startup.java
+++ AriesParts/src/com/cyanogenmod/settings/device/Startup.java
@@ -1,13 +1,19 @@
 package com.cyanogenmod.settings.device;
 
 import android.content.BroadcastReceiver;
+import android.content.SharedPreferences;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 
 public class Startup extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
+        Apply.restore(context);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        int apply = sharedPrefs.getBoolean(DeviceSettings.KEY_APPLY, true) ? 1 : 0;
+	if(apply == 1) {
         ColorTuningPreference.restore(context);
         Mdnie.restore(context);
         TouchKeyBacklightTimeout.restore(context);
@@ -19,6 +25,7 @@ public class Startup extends BroadcastReceiver {
         Wifi.restore(context);
         Charge.restore(context);
         BLXPreference.restore(context);
+	}
     }
 
 }
