749570dac9e1f1013a74055709693402aa4f13c0
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-01-05 17:58:08 +0700

    aries-common: use KitKat zRAM setting
    CM's compcache has been removed, so we need to migrate to KitKat's
    mechanism. The value is set to 64 MB for now, which is approximately
    equal to the old 18% setting.
    
    Change-Id: Ib780aed8c336cea5baf0f672e41179ef85790061
    
    Conflicts:
    
    	device_base.mk

--- device_base.mk
+++ device_base.mk
@@ -161,8 +161,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
        mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
        ro.vold.switchablepair=/mnt/emmc,/mnt/sdcard \
        ro.bq.gpu_to_cpu_unsupported=1 \
-       ro.config.low_ram=false \
-       ro.zram.default=18 \
+       ro.config.low_ram=false
 
 # SGX540 is slower with the scissor optimization enabled
 PRODUCT_PROPERTY_OVERRIDES += \
